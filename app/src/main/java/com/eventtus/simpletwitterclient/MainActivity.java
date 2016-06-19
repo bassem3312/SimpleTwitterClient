c1c15b657e6cbb9a599bf356855bd96489d1a0eb
Bassam <basem3312@gmail.com>
2016-06-19 15:40:32 +0300

    initial Commit

---
+++ app/src/main/java/com/eventtus/simpletwitterclient/MainActivity.java
@@ -0,0 +1,25 @@
+package com.eventtus.simpletwitterclient;
+
+import android.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onPrepareNavigateUpTaskStack(TaskStackBuilder builder) {
+        super.onPrepareNavigateUpTaskStack(builder);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+}
