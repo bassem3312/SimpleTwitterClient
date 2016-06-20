7e1dc6c80e617cfa2a81278246d2371c11abfd59
Bassam <basem3312@gmail.com>
2016-06-21 01:29:41 +0200

    After implementing Splash Screen and checking if user logged-in or not to navigate to login screen or followers screen.

---
+++ app/src/main/java/com/eventtus/simpletwitterclient/views/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.eventtus.simpletwitterclient.views;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.eventtus.simpletwitterclient.R;
+import com.eventtus.simpletwitterclient.helpers.GeneralMethods;
+import com.eventtus.simpletwitterclient.helpers.SharedPreferencesHelper;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Bassam on 6/20/2016.
+ */
+public class SplashScreen extends Activity {
+    private long splashTimerDelay = 4000;
+    private Timer splashTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        splashTimer = new Timer();
+        splashTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                openNextScreen();
+            }
+        }, splashTimerDelay);
+    }
+
+    private void openNextScreen() {
+        if (SharedPreferencesHelper.isLoggedIN(SplashScreen.this)) {
+            GeneralMethods.printLog("====", "ISLogedIN");
+            GeneralMethods.openFollowersActivity(SplashScreen.this);
+        } else {
+            Intent nextIntent = new Intent(SplashScreen.this, LoginActivity.class);
+            // set the new task and clear flags
+            nextIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(nextIntent);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        try {
+            splashTimer.cancel();
+        } catch (Exception ex) {
+        }
+    }
+}
