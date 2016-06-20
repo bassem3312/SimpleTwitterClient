29d079ac1c91289a3711e5ac4df0364d461d57ab
Bassam <basem3312@gmail.com>
2016-06-20 17:33:27 +0200

    After Creating twitter user model

---
+++ app/src/main/java/com/eventtus/simpletwitterclient/Models/TwitterUser.java
@@ -0,0 +1,52 @@
+package com.eventtus.simpletwitterclient.Models;
+
+/**
+ * Created by Bassam on 6/20/2016.
+ */
+public class TwitterUser {
+    private long userID;
+    private String username;
+    private String email;
+    private String profileImageURL;
+    private String backgroundImageURL;
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfileImageURL() {
+        return profileImageURL;
+    }
+
+    public void setProfileImageURL(String profileImageURL) {
+        this.profileImageURL = profileImageURL;
+    }
+
+    public String getBackgroundImageURL() {
+        return backgroundImageURL;
+    }
+
+    public void setBackgroundImageURL(String backgroundImageURL) {
+        this.backgroundImageURL = backgroundImageURL;
+    }
+}
